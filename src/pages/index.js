6d46a712d7369395f256602dcd04858478cd94ea
Alex Reed <aghreed@gmail.com>
2019-02-19 22:54:09 -0600

    initial changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,15 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const navy = `#1B3C59`;
+const lime = `#A6ED8E`;
+const offWhite = `#F2F2F0`;
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1 style={{ color: `${navy}`, textAlign: `center` }}>Alex G.H. Reed</h1>
+    <div style={{ width: `300px`, height: `1px`, backgroundColor: `${navy}`, margin: `0 auto` }}></div>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>

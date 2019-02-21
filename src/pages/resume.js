4bab28892a31ce03a141d74828062b1042534be0
Alex Reed <aghreed@gmail.com>
2019-02-21 13:38:43 -0600

    make work and resume pages outlines

---
+++ src/pages/resume.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Resume = () => (
+  <Layout>
+    <SEO title="Resume" />
+    <h1>Professional Work</h1>
+    {/* Flyover Technical */}
+    {/* Object Partners, Inc */}
+    {/* Aviture, Inc */}
+  </Layout>
+)
+
+export default Resume
---
+++ src/pages/work.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Work = () => (
+  <Layout>
+    <SEO title="Work" />
+    <h1>Outside Nine-to-Five Work</h1>
+    {/* Whiteny's site */}
+    {/* TBG site */}
+    {/* Angular controllerAs syntax talk */}
+    {/* Tornado Party Site */}
+    {/* Conferences attended - React Rally - MidwestJS - NEJS Conf */}
+    {/* Meetups - ngNebraska - NEJS - Mobile Meetup - NodeSchool */}
+    {/* Non-Profit - OpenNebraska */}
+  </Layout>
+)
+
+export default Work
